07becc4436aa01dbfbbd5a7859fb4ce97f1685f5
Janne <jannehyttinen1@gmail.com>
2024-05-31 20:34:56 +0300

    updated heuristic function

--- homemade.py
+++ homemade.py
@@ -31,8 +31,8 @@ class ExampleEngine(MinimalEngine):
 
 CHESS_PIECE_VALUES = {
     chess.PAWN: 100,
-    chess.KNIGHT: 300,
-    chess.BISHOP: 300,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
     chess.ROOK: 500,
     chess.QUEEN: 900,
     chess.KING: 99999
